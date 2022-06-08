0153aa00647c268e0e034bff3b7a6347643380ef
Michal Piorkowski <michal.piorkowski@e-korpi.pl>
2022-06-08 15:32:34 +0200

    Working with Goole Maps

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -8,22 +8,33 @@
   <img src="/images/marker.svg" width="20" height="20" /> {{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}<br/>
   <img src="/images/star.svg" width="20" height="20" />{{ home.reviewValue }}<br/>
   {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath<br/>
+  {{ home.description }}
+  <div style="height:800px;width:800px" ref="map"></div>
 </div>
 </template>
 
 <script>
 import homes from "~/data/homes"
 
+if (process.client) {
+  window.initMap = function() {
+    console.log('test')
+  }  
+}
+
 export default {
   head() {
     return {
-      title: this.home.title
+      title: this.home.title,
     };
   },
   data() {
     return {
       home: {},
     };
+  },
+  mounted() {
+    this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng)
   },
   created() {
     const home = homes.find((home) => home.objectID === this.$route.params.id);
