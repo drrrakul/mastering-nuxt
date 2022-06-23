72f443045ac72600dd3ba7214ce24bde31b0b70f
Michal Piorkowski <michal.piorkowski@e-korpi.pl>
2022-06-23 09:30:44 +0200

    MAS-14 - poprawiony skrypt logowania z uwzględnieniem nowego API google

--- plugins/auth.client.js
+++ plugins/auth.client.js
@@ -20,7 +20,6 @@ export default ({ $config, store }, inject ) => {
   }
 
   function init() {
-    console.log(Cookie.get($config.auth.cookieName))
     window.google.accounts.id.initialize({
       client_id: $config.auth.clientId,
       callback: parseUser,
@@ -29,32 +28,33 @@ export default ({ $config, store }, inject ) => {
       document.getElementById('googleButton'),
       { theme: "outline", size: "medium" }
     );
-    window.google.accounts.id.prompt();
+    
+    if (Cookie.get($config.auth.cookieName)) {
+      console.log('logowanie z cookie')
+      signInVerify()
+    } else {
+      window.google.accounts.id.prompt();
+    }
   }
   
-  async function parseUser(response) {
+  function parseUser(response) {
     const idToken = response.credential;
     Cookie.set($config.auth.cookieName, idToken, { expires: 1/24, sameSite: 'Lax' })
+    signInVerify()
+  }
 
+  async function signInVerify() {
     try {
       const response = await unWrap(await fetch('api/user'))
       const user = response.json
-
-    /**
-     * Wywalone poniższe dwie linie, ponieważ nie dekodujemy już tokena funkcją,
-     * tylko zbieramy dane z naszego middleware (API Algolia proxy)
-     */
-    // const decodedResponse = decodeJwtResponse(response.credential);
-    // const userData = decodedResponse.payload;
-
       store.commit('auth/user', {
         fullName: user.name,
         profileUrl: user.image,
       })
     } catch(error) {
       console.log(error)
+      signOut()
     }
-
   }
 
   function signOut() {
@@ -63,32 +63,4 @@ export default ({ $config, store }, inject ) => {
     store.commit('auth/user', null);
     window.google.accounts.id.prompt();
   }
-
-  function decodeJwtResponse(token) {
-    const segments = token.split('.');
-
-    if (segments.length !== 3) {
-      throw new Error('Not enough or too many segments');
-    }
-
-    // base64 decode and parse JSON
-    const header = JSON.parse(base64urlDecode(segments[0]));
-    const payload = JSON.parse(base64urlDecode(segments[1]));
-
-    return {
-      header: header,
-      payload: payload,
-      signature: segments[2]
-    }
-  }
-
-  function base64urlDecode(str) {
-    return Buffer.from(base64urlUnescape(str), 'base64').toString();
-    // return new Buffer(base64urlUnescape(str), 'base64').toString();
-  };
-  
-  function base64urlUnescape(str) {
-    str += Array(5 - str.length % 4).join('=');
-    return str.replace(/\-/g, '+').replace(/_/g, '/');
-  } 
 }
