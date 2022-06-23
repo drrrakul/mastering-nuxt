9668e9e5cd7737c382d389395e45d9ffb8a54c73
Michal Piorkowski <michal.piorkowski@e-korpi.pl>
2022-06-23 17:12:36 +0200

    MAS-15 Refactoring Admin section and APIs

---
+++ modules/algolia/index.js
@@ -0,0 +1,11 @@
+import getApis from './apis'
+import userRouter from './routers/user'
+
+export default function() {
+  const algoliaConfig = this.options.privateRuntimeConfig.algolia
+  const apis = getApis(algoliaConfig)
+
+  this.nuxt.hook('render:setupMiddleware', app => {
+    app.use('/api/user', userRouter(apis))
+  })
+}
