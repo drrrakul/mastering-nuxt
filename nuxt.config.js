dc7e6a7ac2a826bd97160dff2700c43677ab43d6
Michal Piorkowski <michal.piorkowski@e-korpi.pl>
2022-06-27 17:04:19 +0200

    MAS-16 Uploading Images to Cloudinary (not working, cloudinary account conf. failed)

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,11 +10,17 @@ export default {
       appId: process.env.ALGOLIA_API_ID,
       key: process.env.ALGOLIA_SEARCH_API_KEY,
     },
+    cloudinary: {
+      apiKey: process.env.CLOUDINARY_API_KEY,
+    },
   },
   privateRuntimeConfig: {
     algolia: {
       appId: process.env.ALGOLIA_API_ID,
       key: process.env.ALGOLIA_UPDATE_API_KEY,
+    },
+    cloudinary: {
+      apiSecret: process.env.CLOUDINARY_API_SECRET,
     },
   },
   head: {
@@ -33,9 +39,12 @@ export default {
     prefetchLinks: false,
   },
   plugins: ['~/plugins/maps.client', '~/plugins/dataApi', '~/plugins/auth.client'],
-  modules: ['~/modules/auth', '~/modules/algolia'],
+  modules: ['~/modules/auth', '~/modules/algolia', '~/modules/cloudinary', '@nuxtjs/cloudinary'],
   devServerHandlers: [],
   buildModules: ['@nuxtjs/tailwindcss'],
+  cloudinary: {
+    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  },
   css: ['~/assets/sass/app.scss'],
   build: {
     extractCSS: true,
