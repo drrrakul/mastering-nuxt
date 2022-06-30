67b10a971a60685d25aac5ff26be7e1f50052c3c
drrrakul <107046588+drrrakul@users.noreply.github.com>
2022-06-30 16:00:11 +0200

    MAS-17 - MAS-17-property-availability (#7)
    * MAS-17 date picker from v-calendar added
    
    * MAS-17 Done
    
    Co-authored-by: Michal Piorkowski <michal.piorkowski@e-korpi.pl>

--- pages/admin/homes.vue
+++ pages/admin/homes.vue
@@ -43,6 +43,20 @@
     State: <input type='text' v-model="home.location.state" class="w-26"/><br/>
     Postal Code: <input type='text' v-model="home.location.postalCode" class="w-26"/><br/>
     Country: <input type='text' v-model="home.location.country" class="w-26"/><br/>
+    <date-picker
+      v-for="(range, index) in home.availabilityRanges"
+      :key="index"
+      v-model="home.availabilityRanges[index]"
+      is-range
+      timezone="UTC"
+      :modelConfig="{ timeAdjust: '00:00:00' }"
+    >
+      <template v-slot="{ inputValue, inputEvents }">
+        <input :value="inputValue.start" v-on="inputEvents.start" />
+        to
+        <input :value="inputValue.end" v-on="inputEvents.end" /><br/>
+      </template>
+    </date-picker>
     <button class="border px-4 py-2 border-gray-400">Add</button>    
   </form>
   
@@ -78,6 +92,11 @@ export default {
           lng: "",
         },
         images: [],
+        availabilityRanges: [{
+          start: '', end: '',
+        },{
+          start: '', end: '',
+        }],
       }
     };
   },
