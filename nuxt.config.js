89e44e4b7980f3323a257259a070852b9fade175
drrrakul <107046588+drrrakul@users.noreply.github.com>
2022-06-30 10:56:49 +0200

    MAS-16 - MAS-16-creating-admin-editors (#6)
    * MAS-16 Uploading Images to Cloudinary (not working, cloudinary account conf. failed)
    
    * MAS-16 w trakcie...
    
    * MAS-16 adding & deleting homes - done
    
    * MAS-16 @Nuxt/image done
    
    * MAS-16 NuxtImage module done
    
    Co-authored-by: Michal Piorkowski <michal.piorkowski@e-korpi.pl>

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,11 +10,17 @@ export default {
       appId: process.env.ALGOLIA_API_ID,
       key: process.env.ALGOLIA_SEARCH_API_KEY,
     },
+    cloudinary: {
+      apiKey: process.env.CLOUDINARY_API_KEY,
+    },
   },
   privateRuntimeConfig: {
     algolia: {
       appId: process.env.ALGOLIA_API_ID,
       key: process.env.ALGOLIA_UPDATE_API_KEY,
+    },
+    cloudinary: {
+      apiSecret: process.env.CLOUDINARY_API_SECRET,
     },
   },
   head: {
@@ -33,9 +39,17 @@ export default {
     prefetchLinks: false,
   },
   plugins: ['~/plugins/maps.client', '~/plugins/dataApi', '~/plugins/auth.client'],
-  modules: ['~/modules/auth', '~/modules/algolia'],
+  modules: ['~/modules/auth', '~/modules/algolia', '~/modules/cloudinary', '@nuxtjs/cloudinary'],
   devServerHandlers: [],
-  buildModules: ['@nuxtjs/tailwindcss'],
+  buildModules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
+  cloudinary: {
+    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  },
+  image: {
+    cloudinary: {
+      baseURL: 'https://res.cloudinary.com/'+process.env.CLOUDINARY_CLOUD_NAME+'/image/upload/'
+    }
+  },
   css: ['~/assets/sass/app.scss'],
   build: {
     extractCSS: true,
